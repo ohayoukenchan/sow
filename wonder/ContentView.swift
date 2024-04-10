72f66df51f28931a37e8f7420e8f93b075ba8694
kenta.takano <san.kun.sakana@gmail.com>
2024-04-10 20:09:12 +0900

    Initial Commit

---
+++ wonder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  wonder
+//
+//  Created by ohayoukenchan on 2024/04/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ wonder/wonderApp.swift
@@ -0,0 +1,17 @@
+//
+//  wonderApp.swift
+//  wonder
+//
+//  Created by ohayoukenchan on 2024/04/10.
+//
+
+import SwiftUI
+
+@main
+struct wonderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
