ba6f94337128ebe40e86fdf635a4d7376eeb614a
ohayoukenchan <san.kun.sakana@gmail.com>
2024-06-06 10:53:35 +0900

    test

---
+++ sow/MyView.swift
@@ -0,0 +1,18 @@
+//
+//  MyView.swift
+//  sow
+//
+//  Created by ohayoukenchan on 2024/05/23.
+//
+
+import SwiftUI
+
+struct MyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyView()
+}
---
+++ sow/Packages/Sources/Presentation/UICore/Components/Molecules/CreateProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by ohayoukenchan on 2024/05/22.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
